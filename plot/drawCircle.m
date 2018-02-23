c99a712eba42b6277bf1c39eb24bca1f77071443
Andrew Zaharia <adz213@nyu.edu>
2018-02-23 11:48:12 -0500

    drawCircle: changed axis square to axis equal. also, comments and inconsequential code rearranging

--- plot/drawCircle.m
+++ plot/drawCircle.m
@@ -1,16 +1,17 @@
 function h = drawCircle(center,radius,color,border,alpha,n)
 % h = drawCircle(center,radius,<color='k'>,<border=2>,<alpha=0.2>,<n=100>)
-% center must be Nx2
+% -  center must be Nx2
+% -  accepts multiple inputs and draws multiple circles
 % 
 % 2018-02-22 AZ Created
 
-%% Initialization
+%% Initialize defaults
 if ~exist('color' ,'var') || isempty(color );   color  = [0 0 0];   end
-if ~exist('n'     ,'var') || isempty(n     );   n      = 100;       end
-if ~exist('alpha' ,'var') || isempty(alpha );   alpha  = 0.2;       end
 if ~exist('border','var') || isempty(border);   border = 2;         end
+if ~exist('alpha' ,'var') || isempty(alpha );   alpha  = 0.2;       end
+if ~exist('n'     ,'var') || isempty(n     );   n      = 100;       end
 
-%% Recurse
+%% Recurse if given multiple inputs
 nr = numel(radius);
 nc = numel(center)/2;
 nl = numel(color )/3;
@@ -42,7 +43,7 @@ end
 t = linspace(0,2*pi,n);
 h = fill(radius*cos(t)+center(1),radius*sin(t)+center(2),color);
 set(h,opts{:});
-axis square off
+axis equal off
 hold on
 
 return
