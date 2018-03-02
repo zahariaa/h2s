c5c9552aafbc93921b32bd33e15f124f3179318d
Andrew Zaharia <adz213@nyu.edu>
2018-03-02 16:05:31 -0500

    added plot/showCirclesModel. oops, never committed this

---
+++ plot/showCirclesModel.m
@@ -0,0 +1,20 @@
+function [objHs,XY] = showCirclesModel(model, figPanelSpec, titleStr,nCircPts)
+
+%% control variables
+if ~exist('nCircPts','var') || isempty(nCircPts),   nCircPts = 100;   end
+opacity = 0.7;
+
+%% preparations
+figurePanel(figPanelSpec);
+if ~exist('titleStr','var')
+    titleStr = {'\bfhypersphere2sphere',any2str('error <= ',ceil(model.error*100),'%')};
+end
+title(titleStr);
+
+%% draw circles
+[objHs,XY] = drawCircle(model.centers(:,1:2),model.radii,model.categories.colors,[],opacity,nCircPts);
+% xlabel({any2str('# samples/category: ',model.nSamplesPerCat)});
+axis equal tight off;
+
+return
+
