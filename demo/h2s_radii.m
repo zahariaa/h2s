d2a957b0a77f30c7daa0a3f812695bddbe65696c
Andrew Zaharia <az2522@columbia.edu>
2018-04-16 17:38:29 -0400

    h2s_radii: rearranged/generalized code to allow different comparisons (number of samples as well as number of dimensions)

--- demo/h2s_radii.m
+++ demo/h2s_radii.m
@@ -1,50 +1,76 @@
-function h2s_radii(n,ds,type)
+function [s,target] = h2s_radii(n,d,type,PLOT)
 
-N = 100; % bootstraps
-
-s = NaN(N,5,numel(ds)); % container for sampled estimators
+% Preliminaries
+if ~exist('PLOT','var') || isempty(PLOT),   PLOT = false;   end
 colors = {[1 0 0],[0 1 0],[0 0 1],[1 1 0],[1 0 1]};
-target = NaN(1,numel(ds));
+N = 100; % bootstraps
+s = NaN(N,5); % container for sampled estimators
 
-for d = ds
-   dix = find(d==ds);
-   % Sample & measure radii
-   X  = sampleSpheres(n,d,N,type);
-   radii = sqrt(sum(X.^2,2));
-   maxradii = max(radii,[],1);
-   % Estimators
-   % expDistPerRad method
-   [~,~,tmp] = arrayfun(@(i) estimateHypersphere(X(:,:,i)),1:N,'UniformOutput',false);
-   s(:,1,dix) = cell2mat(tmp);
-   s(:,3,dix) = median(radii,1);
-   s(:,2,dix) = s(:,3,dix).*2^(1/d);   % median*2^(1/d)
-   s(:,4,dix) = maxradii + maxradii/(n*(d-1));                      % MVUE for uniform distribution
-   v = std(reshape(X,[n*d N])); % squeeze(mean(std(X,[],2))); %v(i) = max(diag(cov(X(:,:,i))));
-   s(:,5,dix) = v*sqrt(2)*exp(gammaln((d+1)/2)-gammaln(d/2)); % MVUE for gaussian distribution
-   % Results
-   figure(98);clf;plotEstimators(X,radii,mat2cell(s(:,:,dix),100,ones(1,5)),colors);
-%   skewness(radii(:))
-
-   m(dix,:) = 1-mean(s(:,:,dix),1);
-   if     strcmpi(type,'gaussian'),   target(dix) = median(radii(:));
-   elseif strcmpi(type,'uniform' ),   target(dix) = 1;
+% Recurse if more than one dimension or number of samples input argument given
+nd = numel(d);
+nn = numel(n);
+if nd > 1
+   s = repmat(s,[1 1 nd]);
+   target = NaN(1,nd);
+   for ix = 1:nd
+      [s(:,:,ix),target(ix)] = h2s_radii(n,d(ix),type);
+      if PLOT,   plotRadiusEsts(d,s,target,colors,type);   end
+      xlabel('dimensions')
    end
-   if dix > 1
-      figure(99);cla;hold on;
-      for e = 1:5
-         err = (repmat(target,[N 1])-squeeze(s(:,e,:)))./repmat(target,[N 1]);
-         plotErrorPatch(log2(ds),log10(err.^2),colors{e});
-      end
+   return
+end
+if nn > 1
+   s = repmat(s,[1 1 nn]);
+   target = NaN(1,nn);
+   for ix = 1:nn
+      [s(:,:,ix),target(ix)] = h2s_radii(n(ix),d,type);
+      if PLOT,   plotRadiusEsts(n,s,target,colors,type);   end
+      xlabel('samples')
    end
-   xlabel('dimensions')
-   ylabel('log_{10} error')
-   title([type ' distribution, radius estimation'])
-   legend('','expDistPerRad','','median2^{1/d}',...
-          '','median','','MVUE-Unif','','MVUE-Gauss',...
-          'Location','EastOutside')
+   return
 end
 
+% Sample & measure radii
+X  = sampleSpheres(n,d,N,type);
+radii = sqrt(sum(X.^2,2));
+maxradii = max(radii,[],1);
+% Target to measure estimates against
+if     strcmpi(type,'gaussian'),   target = median(radii(:));
+elseif strcmpi(type,'uniform' ),   target = 1;
+end
+
+% Estimators
+% expDistPerRad method
+[~,~,tmp] = arrayfun(@(i) estimateHypersphere(X(:,:,i)),1:N,'UniformOutput',false);
+s(:,1) = cell2mat(tmp);
+s(:,3) = median(radii,1);
+s(:,2) = s(:,3).*2^(1/d);   % median*2^(1/d)
+s(:,4) = maxradii + maxradii/(n*(d-1));                % MVUE for uniform distribution
+v = std(reshape(X,[n*d N])); % squeeze(mean(std(X,[],2))); %v(i) = max(diag(cov(X(:,:,i))));
+s(:,5) = v*sqrt(2)*exp(gammaln((d+1)/2)-gammaln(d/2)); % MVUE for gaussian distribution
+
+% Results
+if PLOT,   figure(98);clf;plotEstimators(X,radii,mat2cell(s,100,ones(1,5)),colors);   end
+
 return
+
+
+%% PLOT function
+function plotRadiusEsts(ds,s,target,colors,type)
+
+figure(99);cla;hold on;
+for e = 1:5
+   err = (repmat(target,[N 1])-squeeze(s(:,e,:)))./repmat(target,[N 1]);
+   plotErrorPatch(log2(ds),log10(err.^2),colors{e});
+end
+ylabel('log_{10} error')
+title([type ' distribution, radius estimation'])
+legend('','expDistPerRad','','median2^{1/d}',...
+       '','median','','MVUE-Unif','','MVUE-Gauss',...
+       'Location','EastOutside')
+
+return
+end
 
 %% DEMOS/DEBUG
 h2s_radii(200,log2space(1,12,12),'Uniform');xlim([1 12]);
