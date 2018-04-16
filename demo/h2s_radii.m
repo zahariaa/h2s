ad97ba9be5c2d56ffe85c30fafd922f7936f999e
Andrew Zaharia <az2522@columbia.edu>
2018-04-16 16:40:50 -0400

    h2s_radii: fixed MVUEs for both uniform and gaussian case. also, inconsequential variable rename dindex->dix.

--- demo/h2s_radii.m
+++ demo/h2s_radii.m
@@ -6,22 +6,22 @@ s = NaN(N,5,numel(ds)); % container for sampled estimators
 colors = {[1 0 0],[0 1 0],[0 0 1],[1 1 0],[1 0 1]};
 
 for d = ds
-   dindex = find(d==ds);
+   dix = find(d==ds);
    % Sample & measure radii
    X  = sampleSpheres(n,d,N,type);
    radii = sqrt(sum(X.^2,2));
+   maxradii = max(radii,[],1);
    % Estimators
    % expDistPerRad method
    [~,~,tmp] = arrayfun(@(i) estimateHypersphere(X(:,:,i)),1:N,'UniformOutput',false);
-   s(:,1,dindex) = cell2mat(tmp);
-   s(:,3,dindex) = median(radii,1);
-   s(:,2,dindex) = s(:,3,dindex).*2^(1/d);   % median*2^(1/d)
-   maxradii = max(radii,[],1);
-   s(:,4,dindex) = maxradii + maxradii/n;                      % MVUE for uniform distribution
-   s(:,5,dindex) = ones(1,N)*sqrt(2)*exp(gammaln((d+1)/2)-gammaln(d/2)); % MVUE for gaussian distribution
-
+   s(:,1,dix) = cell2mat(tmp);
+   s(:,3,dix) = median(radii,1);
+   s(:,2,dix) = s(:,3,dix).*2^(1/d);   % median*2^(1/d)
+   s(:,4,dix) = maxradii + maxradii/(n*(d-1));                      % MVUE for uniform distribution
+   v = std(reshape(X,[n*d N])); % squeeze(mean(std(X,[],2))); %v(i) = max(diag(cov(X(:,:,i))));
+   s(:,5,dix) = v*sqrt(2)*exp(gammaln((d+1)/2)-gammaln(d/2)); % MVUE for gaussian distribution
    % Results
-   figure(98);clf;plotEstimators(X,radii,mat2cell(s(:,:,dindex),100,ones(1,5)),colors);
+   figure(98);clf;plotEstimators(X,radii,mat2cell(s(:,:,dix),100,ones(1,5)),colors);
 %   skewness(radii(:))
 end
 1-[mn md mx]
