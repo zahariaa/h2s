0afe2c28ee9a701b056b1710f90e5c1e076dc8dd
Andrew Zaharia <az2522@columbia.edu>
2018-04-20 10:52:24 -0400

    hyperdist2dist: now also accepts [samples x dimensions x ... more samples ... ] tensor inputs, e.g. sampleSpheres output

--- tools/hyperdist2dist.m
+++ tools/hyperdist2dist.m
@@ -3,7 +3,17 @@ function lo = hyperdist2dist(hi,dim)
 % Visualizes high dimensional vector magnitudes with
 % magnitudes with 2 (default) or 3 dimensional magnitudes
 % 
+% Assumes input hi is a [samples x dimensions] matrix, or
+% [samples x dimensions x ... more samples ... ] tensor,
+% like sampleSpheres output
+% 
 % 2018-04-19 AZ Created
+
+% If hi is a tensor, reshape it to a matrix
+sz = size(hi);
+if numel(sz)>2
+   hi = reshape(permute(hi,[1 3:numel(sz) 2]),[],sz(2));
+end
 
 if ~exist('dim','var') || isempty(dim),   dim = 2;   end
 
@@ -11,6 +21,11 @@ radii.hi = sqrt(sum(hi         .^2,2));
 radii.lo = sqrt(sum(hi(:,1:dim).^2,2));
 
 lo = repmat(radii.hi,[1 dim]).*hi(:,1:dim)./repmat(radii.lo,[1 dim]);
+
+% If hi was a tensor, reshape lo back to lower dimensional tensor
+if numel(sz)>2
+   lo = permute(reshape(lo,[sz(1) sz(3:end) dim]),[1 numel(sz) 2:(numel(sz)-1)]);
+end
 
 return
 
