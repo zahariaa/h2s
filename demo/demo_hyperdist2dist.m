8cbb4fcfe87b75faace6e9eb9d756bb1042bf38f
Andrew Zaharia <adz213@nyu.edu>
2018-04-20 16:57:00 -0400

    demo_hyperdist2dist: makes two versions of one combined figure with uniform & gaussian distributions, and normalized radius histograms. prints a painters eps and a scatterDensity png

--- demo/demo_hyperdist2dist.m
+++ demo/demo_hyperdist2dist.m
@@ -6,35 +6,57 @@ function demo_hyperdist2dist(lodim)
 % 2018-04-19 AZ Created
 
 %% Preliminaries
-ds = 2.^(1:9); n = 100;
+ds = 2.^(1:2:8); n = 100;
 types = {'Uniform','Gaussian'};
+colors = {[0 0 0],[1 0 0]};
 if ~exist('lodim','var') || isempty(lodim),   lodim = 2;
-elseif lodim==3
-   ds(1) = 3;
+elseif lodim==3                               ds(1) = 3;
+end
+switch lodim
+   case 2,  bign = 10000;
+   case 3,  bign = n;
 end
 
 %% Draw loop
+               figure(1001);set(gcf,'Name',sprintf('%usamples' ,n),'Renderer','painters');
+if lodim==2;   figure(1002);set(gcf,'Name',sprintf('%usamplesD',n),'Renderer','painters');   end
 for t = 1:2
-   figure;set(gcf,'Name',sprintf('%s%usamples',types{t},n));
    i = 0;
    for d = ds
       i = i+1;
       switch t
-         case 1,   X = randnball(n,d);
-         case 2,   X = randn(    n,d);
+         case 1,   X = randnball(bign,d);
+                   r = 1;
+         case 2,   X = randn(    bign,d);
+                   r = sqrt(2)*exp(gammaln((d+1)/2)-gammaln(d/2));
       end
       H = hyperdist2dist(X,lodim);
-      subplot(3,3,i);
+      figure(1001);subplot(3,4,i+(t-1)*4);
       switch lodim
-         case 2,   plot( H(:,1),H(:,2)       ,'ko');drawCircle;
-         case 3,   plot3(H(:,1),H(:,2),H(:,3),'ko');draw3dEllipsoid; lighting gouraud;camlight;
+         case 2,   plot( H(1:n,1),H(1:n,2)         ,'ko'); drawCircle;
+         case 3,   plot3(H(1:n,1),H(1:n,2),H(1:n,3),'ko'); draw3dEllipsoid;
+                   lighting gouraud;camlight;
       end
-      r = sqrt(2)*exp(gammaln((d+1)/2)-gammaln(d/2));
       if     t==2 && lodim==2,   drawCircle(     [],r); 
       elseif t==2 && lodim==3,   draw3dEllipsoid([],r*eye(3));
       end
-      title(sprintf('%u dimensions',d))
+      title(sprintf('%uD %s',d,types{t}))
+      if lodim==2 % 2D "density" histogram of transparent points
+         figure(1002); subplot(3,4,i+(t-1)*4);
+         scatterDensity(H,[1800 1800]/2,true); axis equal off;
+         title(sprintf('%uD %s',d,types{t}))
+      end
+      % Histogram of radii
+      radii = sqrt(sum(X.^2,2));
+      maxbin = max(radii/r)+0.2;
+      bins = 0:0.1:maxbin;
+      h = hist(radii/r,bins);
+      figure(1001); subplot(3,4,i+8); hold on;
+      plot([-flip(bins) bins],[flip(h/sum(h)) h/sum(h)],'Color',colors{t})
+      xlim([-maxbin maxbin])
    end
 end
 
+printFig(1001,[],'eps');
+printFig(1002,[],'png',1200);
 
