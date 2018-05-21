aaf84b8714c4a3cc493de217899637dd1985d796
Andrew Zaharia <adz213@nyu.edu>
2018-05-21 10:37:35 -0400

    demo_swissroll: added meshes of generating manifolds to 3D scatter plots, and in separate plot

--- demo/demo_swissroll.m
+++ demo/demo_swissroll.m
@@ -9,9 +9,12 @@ noise = 0.1;
 t = 3*pi/2 * (1 + 2*rand(N,1));
 r = 5 * rand(N,1);
 
+%% set up meshgrid for mesh versions of plots
+[T,R] = meshgrid(3*pi/2 * linspace(1,3,125),linspace(0,5,5));
+
 %% set up points and categories
 ca.labels  = {'category 1','category 2'};
-ca.colors  = [0.8 0 0; 0 0 0];
+ca.colors  = [0 0 0; 0.8 0 0];
 ca.vectors = logical(blkdiag(true(N,1),true(N,1)));
 
 swissroll  = { @(t,r) [t.*cos(t   ), r, t.*sin(t   )];
@@ -22,9 +25,15 @@ points(ca.vectors(:,1),:) = points(ca.vectors(:,1),:) + swissroll{1}(t,r);
 points(ca.vectors(:,2),:) = points(ca.vectors(:,2),:) + swissroll{2}(t,r);
 
 %% PLOT
-figure;hold on;
-plot3(points(ca.vectors(:,1),1),points(ca.vectors(:,1),2),points(ca.vectors(:,1),3),'ko')
-plot3(points(ca.vectors(:,2),1),points(ca.vectors(:,2),2),points(ca.vectors(:,2),3),'ro')
+figure;set(gcf,'Name','swiss roll');hold on;
+for i = 1:numel(ca.labels)
+   srf = reshape(swissroll{i}(T(:),R(:)),[size(T) 3]);
+   surf(srf(:,:,1),srf(:,:,2),srf(:,:,3),zeros(size(T(:))),...
+        'FaceColor',ca.colors(i,:),'EdgeColor','none','FaceAlpha',0.5)
+   plot3(points(ca.vectors(:,i),1),points(ca.vectors(:,i),2),...
+         points(ca.vectors(:,i),3),'o','MarkerFaceColor',ca.colors(i,:),...
+                                       'MarkerEdgeColor','w')
+end
 axis vis3d off;view(-6,2);draw3Daxes
 
 %% H2S
@@ -55,6 +64,9 @@ end
 figure;hold on;
 set(gcf,'Name','sine waves');
 for i = 1:numel(ca.labels)
+    srf = reshape(sinewaves{i}(T(:),R(:)),[size(T) 3]);
+    surf(srf(:,:,1),srf(:,:,2),srf(:,:,3),zeros(size(T(:))),...
+        'FaceColor',ca.colors(i,:),'FaceAlpha',0.5)
    plot3(points(ca.vectors(:,i),1),points(ca.vectors(:,i),2),...
          points(ca.vectors(:,i),3),'o','MarkerFaceColor',ca.colors(i,:),...
          'MarkerEdgeColor','w')
@@ -67,4 +79,20 @@ figPanelSpec = [h 2 4 1+(i-1)*4];
 titleStr = sprintf('%u points/cat',N);
 HS2SandMDS(points,ca,figPanelSpec,titleStr,dimLow)
 
+%% MESH PLOTS
+figure;set(gcf,'Name','meshes');
+subplot(2,1,1);hold on;
+for i = 1:2
+   srf = reshape(swissroll{i}(T(:),R(:)),[size(T) 3]);
+   surf(srf(:,:,1),srf(:,:,2),srf(:,:,3),zeros(size(T(:))),...
+        'FaceColor',ca.colors(i,:),'EdgeColor','none','FaceAlpha',0.5)
+end
+axis vis3d off;view(-8,-6);draw3Daxes;
+subplot(2,1,2);hold on;
+for i = 1:numel(ca.labels)
+    srf = reshape(sinewaves{i}(T(:),R(:)),[size(T) 3]);
+    surf(srf(:,:,1),srf(:,:,2),srf(:,:,3),zeros(size(T(:))),...
+        'FaceColor',ca.colors(i,:),'EdgeColor','none','FaceAlpha',0.5)
+end
+axis vis3d off;view(88,6);draw3Daxes;
 
