f152e44f6b817baba4e154858f6dd2e0ae06694a
Andrew Zaharia <az2522@columbia.edu>
2018-10-22 19:25:42 -0400

    showSpheresModel: accepts axis handle as input

--- niko/showSpheresModel.m
+++ niko/showSpheresModel.m
@@ -1,15 +1,27 @@
-function showSpheresModel(model, figPanelSpec, titleStr)
+function showSpheresModel(varargin)
 
 %% control variables
 patchDetail = 100;
 opacity = 0.5;
 
-
 %% preparations
-if ~exist('figPanelSpec','var') || isempty(figPanelSpec),   axtivate(gca);
-elseif numel(figPanelSpec)==4,   figurePanel(figPanelSpec);
-elseif numel(figPanelSpec)==1 && ishandle(figPanelSpec), axtivate(figPanelSpec)
+SETCAMERA = true;
+model = varargin{1};
+vv = 1;
+for v = 2:nargin
+   if     islogical(varargin{v}),                   SETCAMERA = varargin{v};
+   elseif ishandle( varargin{v}) && numel(varargin{v})==1, ax = varargin{v};
+   elseif isnumeric(varargin{v}) && numel(varargin{v})==4
+      ax = figurePanel(varargin{v});
+   else
+      if vv==1 && ~exist('ax','var'), ax       = varargin{v}; vv=2;
+      else                            titleStr = varargin{v};
+      end
+   end
 end
+
+if ~exist('ax','var') || isempty(ax), ax = gca; end
+axtivate(ax);
 
 if ~exist('titleStr','var')
     titleStr = any2str('error <= ',ceil(model.error*100),'%');
@@ -23,7 +35,7 @@ covs = arrayfun(@(r) eye(3)*r^2,model.radii,'UniformOutput',false);
 objHs = draw3dEllipsoid(model.centers,covs,model.categories.colors,patchDetail,opacity);
 
 axis equal tight off;
-set(gcf,'Renderer','OpenGL');
+set(get(ax,'Parent'),'Renderer','OpenGL');
 
 
 %% set view and lighting
