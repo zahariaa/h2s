6dd9094e705557d4c748f4d8ec756973b4d78cbd
Andrew Zaharia <az2522@columbia.edu>
2018-10-22 19:16:40 -0400

    figurePanel: outputs axis handle

--- niko/figurePanel.m
+++ niko/figurePanel.m
@@ -1,8 +1,11 @@
-function figurePanel(figPanelSpec)
+function varargout = figurePanel(figPanelSpec)
 
 % create or select figure panel
 figure(figPanelSpec(1)); set(figPanelSpec(1),'Color','w');
 if numel(figPanelSpec)==4
-    subplot(figPanelSpec(2), figPanelSpec(3), figPanelSpec(4));
+    ax = subplot(figPanelSpec(2), figPanelSpec(3), figPanelSpec(4));
 end
 cla; hold on;
+
+if nargout, varargout{1} = ax; end
+
