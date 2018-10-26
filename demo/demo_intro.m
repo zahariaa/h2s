f7d117364257b2e01fd7f79b772579a050b4b3b5
Andrew Zaharia <az2522@columbia.edu>
2018-10-26 13:14:53 -0400

    demo_intro: made first figure based on differently sized spheres with different overlaps

--- demo/demo_intro.m
+++ demo/demo_intro.m
@@ -1,38 +1,46 @@
 % demo_intro: generates introductory explanation figure
 
-n     = 40;
+n     = 80;
 nCats = 4;
 dotsz = 6;
 
 %% Simulate data
 points = randnball(nCats*n,3);
+points = {points(1:n*(nCats-1),:), points};
 % % line of spheres
 % for i = 2:4
-%    points((i-1)*n+1:i*n,1) = points((i-1)*n+1:i*n,1) + i-1;
+%    points{1}((i-1)*n+1:i*n,1) = points{1}((i-1)*n+1:i*n,1) + i-1;
 % end
 % tetrahedron of spheres
-v = [1 0 -1/sqrt(2); -1 0 -1/sqrt(2); 0 1 1/sqrt(2); 0 -1 1/sqrt(2)];
-for i = 1:4
-   points((i-1)*n+1:i*n,:) = points((i-1)*n+1:i*n,:) + repmat(v(i,:),[n 1]);
+v{2} = [1 0 -1/sqrt(2); -1 0 -1/sqrt(2); 0 1 1/sqrt(2); 0 -1 1/sqrt(2)];
+v{1} = v{2}(1:3,:);% + [0.5 0 0;zeros(2,3)]';
+r = {[0.75 1 1.25],[1 1 1 1]};
+for j = 1:2
+   for i = 1:nCats+j-2
+      points{j}((i-1)*n+1:i*n,:) = r{j}(i)*points{j}((i-1)*n+1:i*n,:) ...
+                                   + repmat(v{j}(i,:),[n 1]);
+   end
 end
 
 categories = Categories(n*ones(1,4));
 
 %% PLOT
 planelim = 2;
-for itype = 1:-1:0
-   nCats = size(points,1)/n - itype;
+for itype = 0:1%:-1:0
+   nCats = size(points{itype+1},1)/n - itype;
    fh = newfigure(sprintf('intro%u',itype),[1 3]);
    axtivate(1)
    for i = 1:nCats
-      plot3(points((i-1)*n+1:i*n,1),points((i-1)*n+1:i*n,2),...
-            points((i-1)*n+1:i*n,3),'wo','MarkerSize',dotsz,...
+      plot3(points{itype+1}((i-1)*n+1:i*n,1),points{itype+1}((i-1)*n+1:i*n,2),...
+            points{itype+1}((i-1)*n+1:i*n,3),'wo','MarkerSize',dotsz,...
             'MarkerFaceColor',categories.colors(i,:));
    end
    draw3Daxes([0 0 0],[-1 1 -1 1 -1 1]*planelim); axis vis3d off; view(40,22); rotate3d;
 
    axtivate(2) 
-   sh = draw3dEllipsoid(v(1:nCats,:),[],categories.colors(1:nCats,:),[],1/3);
+   sh = draw3dEllipsoid(v{itype+1},...
+            arrayfun(@(x) eye(3)*x^2,r{itype+1},'UniformOutput',false),...
+            categories.colors(1:nCats,:),[],0.4);
    draw3Daxes([0 0 0],[-1 1 -1 1 -1 1]*planelim); view(40,22);
    match3D(fh.a.h(1),fh.a.h(2));
    set(fh.f,'Renderer','openGL');
@@ -41,12 +49,12 @@ for itype = 1:-1:0
    camlight('left'); lighting phong
 
 %% PLOT PLANE
-   if itype==1
+   if itype==0
       % Compute intersection plane coefficients
-      m = rref([v(1:3,:) ones(3,1)]);
+      m = rref([v{itype+1}(1:3,:) ones(3,1)]);
       m = m(:,end);
       m = -m(1:2)/m(3);
-      b = mean(v(1:3,:));
+      b = mean(v{itype+1}(1:3,:));
       % Draw plane
       [X,Y] = meshgrid([-planelim planelim],[-planelim planelim]);
       X = X([1 2 4 3 1]);
@@ -55,14 +63,17 @@ for itype = 1:-1:0
       X(X>0) = min(planelim,(planelim-b(3))/m(1)); X(X<0) = max(-planelim,(-planelim-b(3))/m(1));
       Y(Y>0) = min(planelim,(planelim-b(3))/m(2)); Y(Y<0) = max(-planelim,(-planelim-b(3))/m(2));
       plane = patch(X+b(1),Y+b(2),m(1)*X+m(2)*Y+b(3),zeros(size(X)),...
-                    'EdgeColor','k','LineWidth',1,'FaceColor',[0.5 0.5 0],'FaceAlpha',0.5)
+                    'EdgeColor','k','LineWidth',1,'FaceColor',[0.5 0.5 0.5],'FaceAlpha',0.5)
       % scaling for intersection circles
       mx = sqrt(1/(1+m(1)^2));
       my = sqrt(1/(1+m(2)^2));
       % Plot intersection circles
       t = linspace(0,2*pi,100);
       for i = 1:nCats
-         circ(i) = plot3(mx*cos(t)+v(i,1),my*sin(t)+v(i,2),v(i,3)+mx*m(1)*cos(t)+my*m(2)*sin(t),...
+         circ(i) = plot3(v{itype+1}(i,1) + r{itype+1}(i)*mx*cos(t),...
+                         v{itype+1}(i,2) + r{itype+1}(i)*my*sin(t),...
+                         v{itype+1}(i,3) + r{itype+1}(i)*mx*cos(t)*m(1) ...
+                                         + r{itype+1}(i)*my*sin(t)*m(2),...
                          'Color',categories.colors(i,:),'LineWidth',2);
       end
    else circ = []; plane = [];
@@ -75,20 +86,33 @@ for itype = 1:-1:0
    delete([sh(:);circ(:);plane]);   % delete spheres, intersection circles, and plane for pdf
 
    for i=1:nCats % Plot centers and radii
-      plot3(v(i,1),v(i,2),v(i,3),'wo','MarkerSize',dotsz,...
+      plot3(v{itype+1}(i,1),v{itype+1}(i,2),v{itype+1}(i,3),'wo','MarkerSize',dotsz,...
             'MarkerFaceColor',categories.colors(i,:));
-      plot3(v(i,1)*[1 1]+[0 1],v(i,2)*[1 1],v(i,3)*[1 1],...
+      plot3(v{itype+1}(i,1)*[1 1]+[0 r{itype+1}(i)],...
+            v{itype+1}(i,2)*[1 1],v{itype+1}(i,3)*[1 1],...
             'Color',categories.colors(i,:))
+   end
+   % Plot lines connecting centers
+   for i = 1:nCats
+      for j = i+1:nCats
+         plot3(v{itype+1}([i j],1),v{itype+1}([i j],2),v{itype+1}([i j],3),'-','Color',[0.5 0.5 0.5])
+      end
    end
    
    axtivate(3)
-   model = hypersphere2sphere(points(1:(n*nCats),:),categories.select(1:nCats),[],2);
+   model = hypersphere2sphere(points{itype+1}(1:(n*nCats),:),categories.select(1:nCats),[],2);
    showCirclesModel(model,[],[]);
    for i = 1:nCats
       plot(model.centers(i,1),model.centers(i,2),'wo','MarkerSize',dotsz,...
            'MarkerFaceColor',categories.colors(i,:))
-      plot(model.centers(i,1)*[1 1]+[0 1],model.centers(i,2)*[1 1],...
-           'Color',categories.colors(i,:))
+      plot(model.centers(i,1)*[1 1]+[0 model.radii(i)],...
+           model.centers(i,2)*[1 1],'Color',categories.colors(i,:))
+   end
+   % Plot lines connecting centers
+   for i = 1:nCats
+      for j = i+1:nCats
+         plot(model.centers([i j],1),model.centers([i j],2),'-','Color',[0.5 0.5 0.5])
+      end
    end
    draw3Daxes;
    papsz = [5.75 5.75/3];
@@ -98,7 +122,7 @@ for itype = 1:-1:0
 end
 
 %% TEST NEW STRESS FUNCTION
-orig = SetOfHyps(v',ones(1,4));
+orig = SetOfHyps(v{2}',ones(1,4));
 low  = SetOfHyps(model);
 low.error = low.stress(orig);
 new  = orig.h2s
