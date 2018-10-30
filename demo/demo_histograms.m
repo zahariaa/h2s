96e4b52e130ac34a645e0317c135399e169b1467
Andrew Zaharia <az2522@columbia.edu>
2018-10-30 14:53:59 -0400

    demo_histograms: added hypercube normalizing factors (by mean distance from center). exact solutions for d=1-4, analytic approximation for the rest

--- demo/demo_histograms.m
+++ demo/demo_histograms.m
@@ -13,6 +13,19 @@ fh = newfigure('histograms',[1 numel(types)]);
 
 % Dimensions to plot
 ds = [1 2 3 4 8 32 128 1024];
+% Hypercube correction factor from: 
+% https://math.stackexchange.com/questions/2446084/distance-from-the-centre-of-a-n-cube-as-n-rightarrow-infty
+% integral2(@(x,y)   sqrt(x.^2 + y.^2)       ,-0.5,0.5,-0.5,0.5)          % d=2
+% integral3(@(x,y,z) sqrt(x.^2 + y.^2 + z.^2),-0.5,0.5,-0.5,0.5,-0.5,0.5) % d=3
+% integral(@(w) integral3(@(x,y,z) sqrt(w.^2 + x.^2 + y.^2 + z.^2),-0.5,0.5,-0.5,0.5,-0.5,0.5),...
+%          -0.5,0.5,'ArrayValued',true) % d=4
+% integral2(@(v,w) arrayfun(@(v,w) integral3(@(x,y,z) sqrt(v.^2 + w.^2 + x.^2 + y.^2 + z.^2),...
+%          -0.5,0.5,-0.5,0.5,-0.5,0.5),v,w),-0.5,0.5,-0.5,0.5) % d=5
+% Don't run this one, it takes forever:
+% integral2(@(s,t) arrayfun(@(s,t) integral3(@(u,v,w) arrayfun(@(u,v,w) ...
+%           integral3(@(x,y,z) sqrt(s.^2 + t.^2 + u.^2 + v.^2 + w.^2 + x.^2 + y.^2 + z.^2),...
+%          -0.5,0.5,-0.5,0.5,-0.5,0.5),u,v,w),-0.5,0.5,-0.5,0.5,-0.5,0.5),s,t),-0.5,0.5,-0.5,0.5) % d=8
+rs = [0.25 0.3826 0.4803 0.5609 sqrt(ds(5:end)/12)];
 
 % Generate perceptually even light gray -> black ...
 cols = flip(makeColorSaturationSeries([0 0 0],numel(ds)));
@@ -24,7 +37,7 @@ for i = 1:numel(ds)
       switch t
       case 1,   r = 1;
       case 2,   r = sqrt(2)*exp(gammaln((d+1)/2)-gammaln(d/2));
-      case 3,   r = 1;
+      case 3,   r = rs(i);
       end
       % Initialize histogram bins, for parallel processing
       h    = zeros(npasses,numel(bins)); 
