1d6cd76800b05033ba6ae1a2a04e83a16db5caa7
Andrew Zaharia <az2522@columbia.edu>
2018-10-30 14:52:34 -0400

    demo_histograms: added hypercube case

--- demo/demo_histograms.m
+++ demo/demo_histograms.m
@@ -3,29 +3,28 @@
 n = 1000000;
 npasses = 100;
 N = 1;
-types = {'Uniform','Gaussian'};
+types = {'Uniform','Gaussian','Hypercubic'};
 maxbin = 2;
 bins = -0.01:0.01:maxbin+0.01;
 
 fp.DESTINATION = {'elife',[1 2]};
 figsetup;fp.txt{4}='Oblique';fp.signif=2;
-fh = newfigure('histograms',[1 2]);
+fh = newfigure('histograms',[1 numel(types)]);
 
 % Dimensions to plot
 ds = [1 2 3 4 8 32 128 1024];
 
 % Generate perceptually even light gray -> black ...
-cols{1} = flip(makeColorSaturationSeries([0 0 0],numel(ds)));
-cols{2} = cols{1};
-cols{2}(:,1) = 1; % ... and light red -> red line colors
+cols = flip(makeColorSaturationSeries([0 0 0],numel(ds)));
 
 for i = 1:numel(ds)
    d = ds(i);
    fprintf('Generating histograms for %u dimensions...\n',d)
-   for t = 1:2
+   for t = 1:numel(types)
       switch t
       case 1,   r = 1;
       case 2,   r = sqrt(2)*exp(gammaln((d+1)/2)-gammaln(d/2));
+      case 3,   r = 1;
       end
       % Initialize histogram bins, for parallel processing
       h    = zeros(npasses,numel(bins)); 
@@ -39,17 +38,18 @@ for i = 1:numel(ds)
       h = sum(h);
       h([1 end])=NaN; % don't plot huge tail
       axtivate(t);
-      %plot(bins,h/sum(h),'Color',cols{t}(i,:),'LineWidth',2);
-      plot(bins(2:end-1),h(2:end-1)/max(h),'Color',cols{t}(i,:),'LineWidth',1);
+      %plot(bins,h/sum(h),'Color',cols(i,:),'LineWidth',2);
+      plot(bins(2:end-1),h(2:end-1)/max(h),'Color',cols(i,:),'LineWidth',1);
    end
 end
 
 %legend(num2str(vectify([ds;ds])))
-legend(num2str(ds(:)));
+%legend(num2str(ds(:)));
 axtivate(1);
 legend(num2str(ds(:)));
-xlabel('Normalized distance from center')
 ylabel('Frequency')
+axtivate(2)
+xlabel('Normalized distance from center')
 %arrayfun(@(a) axesSeparate(a,fp),fh.a.h);
 
 batchPlotRefine(fh,fp);
