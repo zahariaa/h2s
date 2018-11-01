318f99bef6e2a725e5e75c568930f3a7fbf996f2
Andrew Zaharia <az2522@columbia.edu>
2018-10-31 23:08:19 -0400

    Revert "original book demo"
    This reverts commit eab8592f4589b95eea595fd103d25e633fe78c0d.

--- demo/demo_books.m
+++ demo/demo_books.m
@@ -4,18 +4,32 @@ datafile = 'books';
 load(['~' filesep datafile '.mat']);
 
 for i = 1:numel(titles)
-   titles{i}(findstr(titles{i},' ')) = [];
+   titles{i}(strfind(titles{i},' ')) = [];
 end
 
-chapix  = [1 cumsum(chapix)];
+chapix  = [0 cumsum(chapix)];
 vectors = false(size(freq,1),numel(titles));
+newfreq = [];
 for i = 1:numel(titles)
-   vectors(chapix(i):chapix(i+1),i) = true;
+   newfreq = blkdiag(newfreq,squeeze(freq(1+chapix(i):chapix(i+1),:,:)));
+   vectors(1+chapix(i):chapix(i+1),i) = true;
 end
+[words,wix] = sort(words);
+newfreq = newfreq(:,wix);
+
+for i = 1:numel(wix)-1
+   if strcmp(words{i},words{i+1})
+      newfreq(:,i  ) = sum(newfreq(:,i:i+1),2);
+      newfreq(:,i+1) = NaN(chapix(end),1);
+   end
+end
+words = words(~isnan(newfreq(1,:)));
+newfreq = newfreq(:,~isnan(newfreq(1,:)));
 
 cats = Categories(vectors,titles);
 
-hi = SetOfHyps('estimate',freq,cats.select([1:2 5:7]),1);
+hi = SetOfHyps('estimate',newfreq,cats.select([1:2 5:7]),1);
 
 figure;hi.h2s(3).show;cats.select([1:2 5:7]).legend;
+
 
