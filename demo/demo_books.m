6dadcfb0040755083617aba6a3d35c55e74deff9
Andrew Zaharia <az2522@columbia.edu>
2018-11-01 12:17:23 -0400

    ok, leaving this books thing for now. previous version may be better?

--- demo/demo_books.m
+++ demo/demo_books.m
@@ -1,35 +1,50 @@
 % demo_books: h2s on book word frequency
 
+%% Load
 datafile = 'books';
 load(['~' filesep datafile '.mat']);
 
+freq = double(freq);
+%% Strip spaces from legend text (why is this necessary?)
 for i = 1:numel(titles)
    titles{i}(strfind(titles{i},' ')) = [];
 end
-
-chapix  = [0 cumsum(chapix)];
+%% Format vectors
 vectors = false(size(freq,1),numel(titles));
-newfreq = [];
 for i = 1:numel(titles)
-   newfreq = blkdiag(newfreq,squeeze(freq(1+chapix(i):chapix(i+1),:,:)));
    vectors(1+chapix(i):chapix(i+1),i) = true;
 end
-[words,wix] = sort(words);
-newfreq = newfreq(:,wix);
 
-for i = 1:numel(wix)-1
-   if strcmp(words{i},words{i+1})
-      newfreq(:,i  ) = sum(newfreq(:,i:i+1),2);
-      newfreq(:,i+1) = NaN(chapix(end),1);
+%% Merge separate frequency matrices if given
+if ndims(freq)==3
+   newfreq = [];
+   for i = 1:numel(titles)
+      newfreq = blkdiag(newfreq,squeeze(freq(1+chapix(i):chapix(i+1),:,:)));
    end
+   [words,wix] = sort(words);
+   newfreq = newfreq(:,wix);
+   
+   for i = 1:numel(wix)-1
+      if strcmp(words{i},words{i+1})
+         newfreq(:,i  ) = sum(newfreq(:,i:i+1),2);
+         newfreq(:,i+1) = NaN(chapix(end),1);
+      end
+   end
+   words = words(~isnan(newfreq(1,:)));
+   newfreq = newfreq(:,~isnan(newfreq(1,:)));
+   freq = newfreq;
 end
-words = words(~isnan(newfreq(1,:)));
-newfreq = newfreq(:,~isnan(newfreq(1,:)));
+% %% Normalize if using CountVectorizer
+% nw=sum(freq,2);
+% freq(nw~=0,:) = freq(nw~=0,:)./repmat(nw(nw~=0),[1 numel(words)]);
 
+%% COMPUTE
+booksel = 1:6;%[1:3 6:7];
 cats = Categories(vectors,titles);
-
-hi = SetOfHyps('estimate',newfreq,cats.select([1:2 5:7]),1);
-
-figure;hi.h2s(3).show;cats.select([1:2 5:7]).legend;
+hi   = SetOfHyps('estimate',freq,cats.select(booksel),1);
+%% PLOT
+figure; imagesc(freq); colorbar
+figure; hi.h2s(3).show; cats.select(booksel).legend;
+rotate3d
 
 
