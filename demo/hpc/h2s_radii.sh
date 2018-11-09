269a4f19a9a1b1a1c843e3ed661ec5a6f1bc62ca
Andrew Zaharia <az2522@columbia.edu>
2018-11-09 17:07:47 -0500

    h2s_radii.sh: testing writing slurm script file

--- demo/hpc/h2s_radii.sh
+++ demo/hpc/h2s_radii.sh
@@ -21,14 +21,14 @@ basedir="~/results/"
 function="h2s_radii"
 scriptdir=$basedir$function/
 
-# if [ ! -d "$scriptdir" ]
-# then
-#          mkdir $scriptdir
+if [ ! -d "$scriptdir" ]
+then
+         mkdir $scriptdir
 #          mkdir $scriptdir/e
-#          mkdir $scriptdir/m
+         mkdir $scriptdir/m
 #          mkdir $scriptdir/o
-#          mkdir $scriptdir/r
-# fi
+         mkdir $scriptdir/r
+fi
 
 ### BUILD MATLAB JOB
 jobname="fit$jextra"
@@ -48,5 +48,28 @@ else
 fi
 ##### END write matlab code
 
-echo $mfile
+######### construct SLURM (job array) script
+jfile="${scriptdir}$jobname.s"
+
+pbtxt1="#!/bin/bash\n#"
+pbtxt2="#SBATCH --job-name=$jobname"
+pbtxt3="#SBATCH --nodes=1\n#SBATCH --cpus-per-task=$numnodes"
+pbtxt4="#SBATCH --time=$walltime"
+pbtxt5="#SBATCH --mem=$memory"
+pbtxt8="" #SBATCH --output=${scriptdir}o/${jobname}%a.txt"
+pbtxt9="" #SBATCH --error=${scriptdir}e/${jobname}%a.txt"
+pbtxt9a="\nmodule purge\nmodule load matlab/$matlabver\nif [ "'"$SLURM_JOBTMP" == ""'" ]; then\n    export SLURM_JOBTMP=/state/partition1/\$USER/\$\$\n    mkdir -p \$SLURM_JOBTMP\nfi\n\nexport MATLAB_PREFDIR=\$(mktemp -d \$SLURM_JOBTMP/matlab-XXXX)"
+
+# MAIN MATLAB EXECUTION PBS line
+#Command to execute Matlab code
+pbtxt11='matlab -nosplash -nodisplay -nodesktop -r "$mfile" # > matoutfile'
+pbtxt10=""
+pbtxt12=""
+
+# WRITE SLURM SCRIPT
+echo -e "${pbtxt1}\n${pbtxt2}\n${pbtxt3}\n${pbtxt4}\n${pbtxt5}\n${pbtxt8}\n${pbtxt9}\n${pbtxt9a}\n\n${pbtxt10}\n${pbtxt11}\n${pbtxt12}\n" > ${jfile}
+
+# submit array job
+jid=`sbatch $jfile`
+#echo $jid
 
