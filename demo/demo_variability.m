26ea657d9f84feddf266edc352651a689390b173
Andrew Zaharia <az2522@columbia.edu>
2019-10-07 15:37:04 -0400

    added demo_variability: cloned from demo_random

---
+++ demo/demo_variability.m
@@ -0,0 +1,56 @@
+% demo_random
+
+seed = rng(0);
+SIMPLICES = false; %true;
+another3  = false;
+
+if SIMPLICES
+   ds = [3 5 7 3];
+else
+   ds = [3 3 4 4 6 6 9 9];
+end
+
+nconditions = 5;
+fh = newfigure('n-random',[numel(ds) nconditions]);
+set(fh.f,'Units','normalized');
+
+i = 0;
+for d = ds
+   i = i+1;
+   j = 1;
+   dimLow = min(d-1,3);
+   if SIMPLICES
+      if     d==3 &&  another3
+         groundtruth = SetOfHyps([nsimplex(d)';nsimplex(d)'+3],ones(2*d+2,1));
+      else
+         another3    = true;
+         groundtruth = SetOfHyps(nsimplex(d)',ones(d+1,1));
+      end
+   else
+      groundtruth = SetOfHyps(2*rand(d+1,d),rand(d+1,1));
+   end
+   [points,categories] = randnsimplex_of_nballs(groundtruth.centers,50,...
+                                                groundtruth.radii);
+   [model,high] = hypersphere2sphere(points,categories,[],dimLow);
+   model = SetOfHyps(model,groundtruth);
+   model.sig = model.significance(points,1000);
+   %keyboard
+   testhi = SetOfHyps('estimate',points,categories);%,100).merge;
+   [~,~,testhi.error,testhi.msflips] = testhi.stress(groundtruth);
+   testhi.sig = testhi.significance(points);
+   axtivate(fh.a.h((i-1)*nconditions+j));  model.show;
+
+   for CONSTRAINT = [false true]
+      for FIXRADII = [false true]
+         j = j+1;
+         testlo = testhi.h2s(dimLow,[FIXRADII CONSTRAINT]);
+         [~,~,testlo.error,testlo.msflips] = testlo.stress(groundtruth);
+         testlo.sig = testlo.significance(points);
+      
+         axtivate(fh.a.h((i-1)*nconditions+j)); testlo.show;
+         drawnow;
+      end
+   end
+   %[groundtruth.overlap;model.overlap;testlo.overlap]
+end
+
