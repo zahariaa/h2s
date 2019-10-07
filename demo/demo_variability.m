3387649329c08d6c94ff427e1582ded13aa23b3c
Andrew Zaharia <az2522@columbia.edu>
2019-10-07 15:38:06 -0400

    demo_variability: tests variability of h2s initializations, with and without using MDS as a starting point. this version is for fixed radii

--- demo/demo_variability.m
+++ demo/demo_variability.m
@@ -1,56 +1,59 @@
-% demo_random
+% demo_variability
 
 seed = rng(0);
 SIMPLICES = false; %true;
 another3  = false;
+CONSTRAINT= false;
+FIXRADII  = true;
 
 if SIMPLICES
    ds = [3 5 7 3];
 else
-   ds = [3 3 4 4 6 6 9 9];
+   ds = [3 4 6 9];
 end
 
 nconditions = 5;
-fh = newfigure('n-random',[numel(ds) nconditions]);
+fh = newfigure('n-random',[numel(ds)*2 nconditions]);
 set(fh.f,'Units','normalized');
 
 i = 0;
 for d = ds
-   i = i+1;
-   j = 1;
    dimLow = min(d-1,3);
-   if SIMPLICES
-      if     d==3 &&  another3
-         groundtruth = SetOfHyps([nsimplex(d)';nsimplex(d)'+3],ones(2*d+2,1));
-      else
-         another3    = true;
-         groundtruth = SetOfHyps(nsimplex(d)',ones(d+1,1));
-      end
-   else
-      groundtruth = SetOfHyps(2*rand(d+1,d),rand(d+1,1));
-   end
-   [points,categories] = randnsimplex_of_nballs(groundtruth.centers,50,...
-                                                groundtruth.radii);
-   [model,high] = hypersphere2sphere(points,categories,[],dimLow);
-   model = SetOfHyps(model,groundtruth);
-   model.sig = model.significance(points,1000);
-   %keyboard
-   testhi = SetOfHyps('estimate',points,categories);%,100).merge;
-   [~,~,testhi.error,testhi.msflips] = testhi.stress(groundtruth);
-   testhi.sig = testhi.significance(points);
-   axtivate(fh.a.h((i-1)*nconditions+j));  model.show;
 
-   for CONSTRAINT = [false true]
-      for FIXRADII = [false true]
-         j = j+1;
-         testlo = testhi.h2s(dimLow,[FIXRADII CONSTRAINT]);
+   for j = 1:nconditions
+      if SIMPLICES
+         if     d==3 &&  another3
+            groundtruth = SetOfHyps([nsimplex(d)';nsimplex(d)'+3],ones(2*d+2,1));
+         else
+            another3    = true;
+            groundtruth = SetOfHyps(nsimplex(d)',ones(d+1,1));
+         end
+      else
+         groundtruth = SetOfHyps(2*rand(d+1,d),rand(d+1,1));
+      end
+      [points,categories] = randnsimplex_of_nballs(groundtruth.centers,50,...
+                                                   groundtruth.radii);
+   
+      [model,high] = hypersphere2sphere(points,categories,[],dimLow);
+      model = SetOfHyps(model,groundtruth);
+      model.sig = model.significance(points,1000);
+      %keyboard
+      testhi = SetOfHyps('estimate',points,categories);%,100).merge;
+      [~,~,testhi.error,testhi.msflips] = testhi.stress(groundtruth);
+      testhi.sig = testhi.significance(points);
+
+      %axtivate(fh.a.h(i*2*nconditions+j));  model.show;
+
+      for MDS_INIT = [false true]
+         testlo = testhi.h2s(dimLow,[FIXRADII CONSTRAINT MDS_INIT]);
          [~,~,testlo.error,testlo.msflips] = testlo.stress(groundtruth);
          testlo.sig = testlo.significance(points);
       
-         axtivate(fh.a.h((i-1)*nconditions+j)); testlo.show;
+         axtivate(fh.a.h((i*2+double(MDS_INIT))*nconditions+j)); testlo.show;
          drawnow;
       end
    end
    %[groundtruth.overlap;model.overlap;testlo.overlap]
+   i = i+1;
 end
 
