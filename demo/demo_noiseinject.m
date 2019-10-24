c31dba24662cc137f06dbbd0c4e760d4664320de
Andrew Zaharia <az2522@columbia.edu>
2019-10-24 11:52:57 -0400

    demo_noiseinject: doesn't actually inject noise yet, but prints distance and total errors, comparing using MDS initialization or not

--- demo/demo_noiseinject.m
+++ demo/demo_noiseinject.m
@@ -1,7 +1,7 @@
-% demo_variability
+% demo_noiseinject
 
-seed = rng(0);
-SIMPLICES = false;%true;
+seed = rng(1);
+SIMPLICES = true;
 another3  = false;
 CONSTRAINT= false;
 FIXRADII  = true;
@@ -13,47 +13,37 @@ else
    ds = [3 4 6 9];
 end
 
-nconditions = 5;
-fh = newfigure('n-random',[numel(ds)*2 nconditions]);
-set(fh.f,'Units','normalized');
+% fh = newfigure('n-random',[numel(ds) nconditions]);
+% set(fh.f,'Units','normalized');
 
-i = 0;
 for d = ds
-   dimLow = min(d-1,3);
-
-   for j = 1:nconditions
-      if SIMPLICES
-         if     d==3 &&  another3
-            groundtruth = SetOfHyps([nsimplex(d)';nsimplex(d)'+3],ones(2*d+2,1));
-         else
-            another3    = true;
-            groundtruth = SetOfHyps(nsimplex(d)',ones(d+1,1));
-         end
+   if SIMPLICES
+      if     d==3 &&  another3
+         groundtruth = SetOfHyps([nsimplex(d)';nsimplex(d)'+3],ones(2*d+2,1));
       else
-         groundtruth = SetOfHyps(2*rand(d+1,d),rand(d+1,1));
+         another3    = true;
+         groundtruth = SetOfHyps(nsimplex(d)',ones(d+1,1));
       end
-      [points,categories] = randnsimplex_of_nballs(groundtruth.centers,50,...
-                                                   groundtruth.radii);
-   
-      [model,high] = hypersphere2sphere(points,categories,[],dimLow);
-      model = SetOfHyps(model,groundtruth);
-      model.sig = model.significance(points,1000);
-      %keyboard
-      testhi = SetOfHyps('estimate',points,categories);%,100).merge;
-      [~,~,testhi.error,testhi.msflips] = testhi.stress(groundtruth);
-      testhi.sig = testhi.significance(points);
-   
-      %axtivate(fh.a.h(i*2*nconditions+j));  model.show;
-      for MDS_INIT = [false true]
-         testlo = testhi.h2s([dimLow 0 ninits],[FIXRADII CONSTRAINT MDS_INIT]);
-         [~,~,testlo.error,testlo.msflips] = testlo.stress(groundtruth);
-         testlo.sig = testlo.significance(points);
-      
-         axtivate(fh.a.h((i*2+double(MDS_INIT))*nconditions+j)); testlo.show;
-         drawnow;
-      end
+   else
+      groundtruth = SetOfHyps(2*rand(d+1,d),rand(d+1,1));
    end
-   %[groundtruth.overlap;model.overlap;testlo.overlap]
-   i = i+1;
+   [points,categories] = randnsimplex_of_nballs(groundtruth.centers,50,...
+                                                groundtruth.radii);
+
+   hi = SetOfHyps('estimate',points,categories);%,100).merge;
+   rfixed = hi.h2s(true); % MDS_INIT & FIXEDRADII=true
+   rfixed.sig = rfixed.significance(points);
+
+   rfree  = rfixed.h2s(hi,false);
+   rfree.sig = rfree.significance(points);
+
+   % All error
+   [sum(rfixed.error) sum(rfree.error)]
+   % center errors only
+   nc2 = nchoosek(numel(hi.radii),2);
+   [sum(rfixed.error(1:nc2)) sum(rfree.error(1:nc2))]
+
+   %axtivate(fh.a.h((i*2+double(MDS_INIT))*nconditions+j)); lo.show;
+   %drawnow;
 end
 
