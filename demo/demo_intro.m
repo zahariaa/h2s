824459259b325554716c8f79e51f89808abb3fad
Andrew Zaharia <az2522@columbia.edu>
2019-11-26 16:21:27 -0500

    demo_intro: changes to make a new stats explanation figure

--- demo/demo_intro.m
+++ demo/demo_intro.m
@@ -1,35 +1,37 @@
 % demo_intro: generates introductory explanation figure
 
-n     = 80;
+n     = 50;
 nCats = 4;
 dotsz = 6;
 
 %% Simulate data
-points = randnball(nCats*n,3);
-points = {points(1:n*(nCats-1),:), points};
 % % line of spheres
 % for i = 2:4
 %    points{1}((i-1)*n+1:i*n,1) = points{1}((i-1)*n+1:i*n,1) + i-1;
 % end
 % tetrahedron of spheres
-v{2} = [1 0 -1/sqrt(2); -1 0 -1/sqrt(2); 0 1 1/sqrt(2); 0 -1 1/sqrt(2)];
-v{1} = v{2}(1:3,:);% + [0.5 0 0;zeros(2,3)]';
-v{1}(2,3)=-0.5;
-r = {[0.75 1 1.25],[1 1 1 1]};
-for j = 1:2
-   for i = 1:nCats+j-2
+v{1} = [9 0 0; [9 9]/sqrt(2) 0; [4 4]/sqrt(2) 0; 0 0 0] - [1;1;1;1]*[6 0 0];
+r = {1:4,ones(1,6)/sqrt(3/2)};
+
+%%TODO: generalize simplex scale factor to have 0 overlap
+%d=3;hi = SetOfHyps(nsimplex(d)*sqrt(3/2),ones(d+1,1));hi.margins
+v{2} = nsimplex(3)';
+v{2} = v{2}([1:3 2:4],:) + [[3;3;3;-3;-3;-3] zeros(6,2)];
+
+points = cellfun(@(x) randnball(n*numel(x),3),r,'UniformOutput',false);
+for j = 1:numel(r)
+   for i = 1:numel(r{j})
       points{j}((i-1)*n+1:i*n,:) = r{j}(i)*points{j}((i-1)*n+1:i*n,:) ...
                                    + repmat(v{j}(i,:),[n 1]);
    end
 end
 
-categories = Categories(n*ones(1,4));
 
 %% PLOT
 planelim = 2;
-for itype = 1:-1:0
+for itype = 0%1%1:-1:0
    orig = SetOfHyps(v{itype+1},r{itype+1});
-   orig.categories = categories;
+   orig.categories = Categories(n*ones(1,numel(r{itype+1})));
 
    nCats = size(points{itype+1},1)/n;
    fh = newfigure(sprintf('intro%u',itype),[1 3]);
@@ -37,15 +39,15 @@ for itype = 1:-1:0
    for i = 1:nCats
       plot3(points{itype+1}((i-1)*n+1:i*n,1),points{itype+1}((i-1)*n+1:i*n,2),...
             points{itype+1}((i-1)*n+1:i*n,3),'wo','MarkerSize',dotsz,...
-            'MarkerFaceColor',categories.colors(i,:));
+            'MarkerFaceColor',orig.categories.colors(i,:));
    end
-   draw3Daxes([0 0 0],[-1 1 -1 1 -1 1]*planelim); axis vis3d off; view(40,22); rotate3d;
+   draw3Daxes([0 0 0],5*[-1 1 -1 1 -1 1]*planelim); axis equal vis3d off; view(40,22); rotate3d;
 
    axtivate(2) 
    sh = draw3dEllipsoid(v{itype+1},...
             arrayfun(@(x) eye(3)*x^2,r{itype+1},'UniformOutput',false),...
-            categories.colors(1:nCats,:),[],0.4);
-   draw3Daxes([0 0 0],[-1 1 -1 1 -1 1]*planelim); view(40,22);
+            orig.categories.colors(1:nCats,:),[],0.4);
+   draw3Daxes([0 0 0],5*[-1 1 -1 1 -1 1]*planelim); view(40,22);
    match3D(fh.a.h(1),fh.a.h(2));
    set(fh.f,'Renderer','openGL');
    axis(fh.a.h(3),'off');
@@ -78,7 +80,7 @@ for itype = 1:-1:0
                          v{itype+1}(i,2) + r{itype+1}(i)*my*sin(t),...
                          v{itype+1}(i,3) + r{itype+1}(i)*mx*cos(t)*m(1) ...
                                          + r{itype+1}(i)*my*sin(t)*m(2),...
-                         'Color',categories.colors(i,:),'LineWidth',2);
+                         'Color',orig.categories.colors(i,:),'LineWidth',2);
       end
    else circ = []; plane = [];
    end
@@ -91,10 +93,10 @@ for itype = 1:-1:0
 
    for i=1:nCats % Plot centers and radii
       plot3(v{itype+1}(i,1),v{itype+1}(i,2),v{itype+1}(i,3),'wo','MarkerSize',dotsz,...
-            'MarkerFaceColor',categories.colors(i,:));
+            'MarkerFaceColor',orig.categories.colors(i,:));
       plot3(v{itype+1}(i,1)*[1 1]+[0 r{itype+1}(i)],...
             v{itype+1}(i,2)*[1 1],v{itype+1}(i,3)*[1 1],...
-            'Color',categories.colors(i,:))
+            'Color',orig.categories.colors(i,:))
    end
    % Plot lines connecting centers
    for i = 1:nCats
@@ -104,14 +106,14 @@ for itype = 1:-1:0
    end
    
    axtivate(3)
-   model = hypersphere2sphere(points{itype+1}(1:(n*nCats),:),categories.select(1:nCats),[],2);
+   model = hypersphere2sphere(points{itype+1}(1:(n*nCats),:),orig.categories.select(1:nCats),[],2);
    [~,~,model.error] = SetOfHyps(model).stress(orig);
    showCirclesModel(model,[],[]);
    for i = 1:nCats
       plot(model.centers(i,1),model.centers(i,2),'wo','MarkerSize',dotsz,...
-           'MarkerFaceColor',categories.colors(i,:))
+           'MarkerFaceColor',orig.categories.colors(i,:))
       plot(model.centers(i,1)*[1 1]+[0 model.radii(i)],...
-           model.centers(i,2)*[1 1],'Color',categories.colors(i,:))
+           model.centers(i,2)*[1 1],'Color',orig.categories.colors(i,:))
    end
    % Plot lines connecting centers
    for i = 1:nCats
@@ -138,14 +140,23 @@ new.sig = orig.sig;
 fh = newfigure([3 2],'compare'); low.show(fh.a.h(1));
                                  new.show(fh.a.h(2));
 set(fh.f,'Renderer','openGL')
-orig.showSig(orig.sig,fh.a.h(5));
+orig.showSig(fh.a.h(5),'legend');
 orig.showSig(sec,fh.a.h(6));
-orig.showSigLegend(fh.a.h(5));
 matchy(fh.a.h(5:6))
 
 low.showValues(fh.a.h(3))
 new.showValues(low,fh.a.h(4))
 
+% Print
+set(fh.f,'Renderer','painters')
+printFig(fh.f,[],'eps')
+
+fh = newfigure([1 4],'stats');
+new.show(fh.a.h(1));
+new.showValues(low,fh.a.h(2))
+orig.showSig(fh.a.h(3));
+orig.showSig(sec,fh.a.h(4));
+matchy(fh.a.h(2:4))
 % Print
 set(fh.f,'Renderer','painters')
 printFig(fh.f,[],'eps')
