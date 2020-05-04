a1efb96069616ed29ff490fdc36c369589caf07a
Andrew Zaharia <az2522@columbia.edu>
2020-05-04 16:10:43 -0400

    estimateHypersphere: allow "distance" as an estimator input option. if a distance matrix is provided, automatically detect and set relevant settings. de-squareform it if it is a full symmetric matrix, and if bootstrapping, force permutation mode. other changes are inconsequential code reorganization to prep for adding more estimators

--- niko/estimateHypersphere.m
+++ niko/estimateHypersphere.m
@@ -19,15 +19,6 @@ ESTIMATOR  = '';
 DBUGPLOT   = false;
 VERBOSE    = false;
 
-% Auto-detect distance matrix (do easy/fast checks first)
-if f==1 && n==d && all(diag(points)==0) && issymmetric(points)
-   DISTMATRIX = true;
-   normtype   = [];    % if normalizing, make full matrix min be 0 and max be 1
-else
-   DISTMATRIX = false;
-   normtype   = 'col'; % if normalizing, adjust each column by overall mean and std
-end
-
 for v = 2:nargin
    if isa(varargin{v-1},'Categories'), categories        = varargin{v-1};
    elseif isnumeric(varargin{v-1}),    nBootstrapSamples = varargin{v-1};
@@ -37,16 +28,29 @@ for v = 2:nargin
          case 'stratified';  STRATIFIED = true;
          case 'normalize';   NORMALIZE  = true;
          case 'raw';         NORMALIZE  = false;
-         otherwise,          ESTIMATOR  = varargin{v-1};
+         otherwise,          ESTIMATOR  = lower(varargin{v-1});
       end
    end
+end
+
+% Auto-detect distance matrix (do easy/fast checks first)
+if f==1 && ( (n==d && all(diag(points)==0) && issymmetric(points)) ...
+   || strcmp(ESTIMATOR,'distance') || d==1 )% this line assumes points are de-squareform'd
+   DISTMATRIX = true;
+   normtype   = [];    % if normalizing, make full matrix min be 0 and max be 1
+   ESTIMATOR  = 'distance'; % override estimator choice
+   STRATIFIED = false; % bootstrap must be w/o replacement; stratified doesn't make sense
+   if n==d, points = squareform(points); end
+else
+   DISTMATRIX = false;
+   normtype   = 'col'; % if normalizing, adjust each column by overall mean and std
 end
 
 if f==1 && NORMALIZE, points = normalize(points,normtype); end
 
 %% recurse cell array of points
 if exist('categories','var') && numel(categories.labels)>1
-%% EVALUATE ALL FRAMES IN SAME SPACE
+   %% EVALUATE ALL FRAMES IN SAME SPACE
    if f > 1
       points  = reshape(permute(points,[1 3 2]),n*f,d);
       if NORMALIZE
@@ -64,7 +68,7 @@ if exist('categories','var') && numel(categories.labels)>1
       return
    end
    %% permutation or stratified bootstrap test
-   if nBootstrapSamples > 1 && ~strcmpi(ESTIMATOR,'mcmc')
+   if nBootstrapSamples > 1 && ~strcmp(ESTIMATOR,'mcmc')
       catperm = [categories; categories.permute(nBootstrapSamples,STRATIFIED)];
       % Recursive call
       hyp = arrayfun(@(c) estimateHypersphere(points,c,'raw',ESTIMATOR),catperm);
@@ -79,21 +83,23 @@ if exist('categories','var') && numel(categories.labels)>1
       % Recursive call
       hyp = cellfun(@(x) estimateHypersphere(x,'raw',ESTIMATOR,nBootstrapSamples),...
                p,'UniformOutput',false);
-      if strcmpi(ESTIMATOR,'mcmc')
-         % do some fancy combining
-         hyp = cellfun(@unconcat, hyp,'UniformOutput',false); % n cell of nSamples
-         hyp = num2cell(cell2mat_concat(hyp),1); % nSamples cell of n Hyperspheres
-         hyp = cellfun(@(x) x.concat(categories),hyp); % nSamples of Hyperspheres
-      else
-         hyp = cell2mat_concat(hyp).concat(categories);
+      switch(ESTIMATOR)
+         case 'mcmc'
+            % do some fancy combining
+            hyp = cellfun(@unconcat, hyp,'UniformOutput',false); % n-cell of nSamples
+            hyp = num2cell(cell2mat_concat(hyp),1); % nSamples-cell of n-Hyperspheres
+            hyp = cellfun(@(x) x.concat(categories),hyp); % nSamples n-Hyperspheres
+         otherwise
+            hyp = cell2mat_concat(hyp).concat(categories);
       end
    end
    return
 end
 
-switch(lower(ESTIMATOR))
+%% DO THE ACTUAL ESTIMATION
+switch(ESTIMATOR)
    case 'mcmc'
-      [loc,rad,llh] = inferHyperspherePosterior(points,nBootstrapSamples,DBUGPLOT,VERBOSE);
+      [loc,rad] = inferHyperspherePosterior(points,nBootstrapSamples,DBUGPLOT,VERBOSE);
    otherwise
       %% estimate location
       loc = mean(points,1); % optimises L2 cost, corresponds to L1 force equilibrium
@@ -112,6 +118,7 @@ switch(lower(ESTIMATOR))
       end
 end
 
+%% OUTPUT FORMATTING
 if ~exist('categories','var')
    categories = Categories(numel(rad));
 end
@@ -122,7 +129,7 @@ return
 end
 
 
-
+%% HELPER FUNCTIONS
 function v = stdPer(d)
 expectedStds = [1.2733    1.0115    0.8796    0.8107    0.8384    0.8638    0.9579    1.0403    1.1938  1.4268    1.8384    2.4485];
 v = interp1(2.^(1:12),expectedStds,d);
