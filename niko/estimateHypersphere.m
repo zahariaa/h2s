ace838d292e11560d8c96498991ca78885675601
Andrew Zaharia <az2522@columbia.edu>
2020-05-06 18:00:52 -0400

    estimateHypersphere: re-arranged code, but no functional impact. preparing to include more estimators

--- niko/estimateHypersphere.m
+++ niko/estimateHypersphere.m
@@ -36,10 +36,12 @@ end
 % Auto-detect distance matrix (do easy/fast checks first)
 if f==1 && ( (n==d && all(diag(points)==0) && issymmetric(points)) ...
    || strcmp(ESTIMATOR,'distance') || d==1 )% this line assumes points are de-squareform'd
+
    DISTMATRIX = true;
    normtype   = [];    % if normalizing, make full matrix min be 0 and max be 1
    ESTIMATOR  = 'distance'; % override estimator choice
    STRATIFIED = false; % bootstrap must be w/o replacement; stratified doesn't make sense
+
    if n==d, points = squareform(points); end
 else
    DISTMATRIX = false;
@@ -77,21 +79,21 @@ if exist('categories','var') && numel(categories.labels)>1
          % build inter-category mean distance matrix
          nCats = numel(categories.labels);
          catix = nchoosek_ix(nCats);
-         distsAcross = zeros(1,nchoosek(nCats,2));
+         distsAcross = zeros(1,size(catix,2));
          for i = 1:size(catix,2)
             distsAcross(i) = mean(points( ...
                               categories.select(catix(:,i)).vectorsForDistanceMatrix ));
          end
-         % collect within-category distances, so later recursive calls compute radii
-         for i = 1:nCats
-            p{i} = points( categories.select(i).vectorsForDistanceMatrix );
-         end
-         % not doing this anymore: compute centers from all unique distances
-
          % mds on the category-level distance matrix to get centers
          % TODO: what should the dimensionality of the high-dimensional embedding be?
          nDimsEmbedding = max(2,nCats-1);
          centers = mdscale(distsAcross,nDimsEmbedding,'Criterion','metricstress');
+         % not doing this anymore: compute centers from all unique distances
+
+         % collect within-category distances, so later recursive calls compute radii
+         for i = 1:nCats
+            p{i} = points( categories.select(i).vectorsForDistanceMatrix );
+         end
       else
          for i = 1:numel(categories.labels)
             ix = categories.select(i).vectors;
@@ -123,27 +125,32 @@ if exist('categories','var') && numel(categories.labels)>1
 end
 
 %% DO THE ACTUAL ESTIMATION
+%% estimate location, re-center points
 switch(ESTIMATOR)
-   case 'mcmc'
-      [loc,rad] = inferHyperspherePosterior(points,nBootstrapSamples,DBUGPLOT,VERBOSE);
+   case 'distance' % do nothing
+   case 'mcmc'     % do nothing
+   otherwise       loc    = mean(points,1); % optimises L2 cost (same as L1 force equilibrium)
+                   points = points - repmat(loc,[n 1]);
+end
+
+switch(ESTIMATOR)
    case 'distance'
       loc = []; % consistent center points are in a lower call
       % points here are actually distances
       rad = mean(points)/expDistPerRad(d);
+   case 'mcmc'
+      [loc,rad] = inferHyperspherePosterior(points,nBootstrapSamples,DBUGPLOT,VERBOSE);
    otherwise
-      %% estimate location
-      loc = mean(points,1); % optimises L2 cost, corresponds to L1 force equilibrium
       % dists = pdist(points,'Euclidean');
       % cv = cvindex(n,10);
       % loc = mean(cell2mat_concat(cv.crossvalidate(@mean,points)));
 
       %% estimate radius via skew-based MVUEs
-      points = points - repmat(loc,[n 1]);
       radii = sqrt(sum(points.^2,2));
       dvarrad = log2(std(radii/median(radii)))+log2(d)/1.5;
       if dvarrad > -0.5 % Assume Gaussian
-         rad = sqrt(sum(var(points,[],2))/(n-1))*sqrt(2)*exp(gammaln((d+1)/2)-gammaln(d/2));
-      else             % Assume Uniform
+         rad = mvue_gaussian(points,d,n);
+      else              % Assume Uniform
          rad = stdPer(d)*std(radii) + median(radii);
       end
 end
@@ -176,3 +183,11 @@ else         r = sqrt(2);
 end
 return
 end
+
+function rad = mvue_gaussian(points,d,n)
+   target    = sqrt(2)*exp(gammaln((d+1)/2)-gammaln(d/2));
+   % rad       = std(points)*target;
+   rad       = sqrt(sum(var(points,[],2))/(n-1))*target;
+   return
+end
+
