7d7893f7d26cc4425746d9c73f04ba39e87057de
Andrew Zaharia <az2522@columbia.edu>
2020-05-29 17:38:34 -0400

    azbox changes: matchy: made input handling way more flexible/intuitive. uses varargin and loop instead of recursion. Added support for color axis matching ('c'/'CLim' option) axtivate: almost total rewrite. made input handling more flexible. added functionality: property value pairs provided to axtivate are applied to the axis being activated. e.g. axtivate(5,'XScale','log') activates the axis in fh.a.h(5) and sets its x-axis scale to log

--- other/azbox/axtivate.m
+++ other/azbox/axtivate.m
@@ -1,4 +1,4 @@
-function axtivate(a,f)
+function axtivate(a,varargin)
 % axtivate(a,f): activates axis in fh.a(f).h(a)
 % assumes f is the last one available if not specified.
 % pulls fh in workspace from calling function.
@@ -7,17 +7,29 @@ function axtivate(a,f)
 % 
 % 2016-08-04 AZ Created
 
+vstart = 1;
+
 if ishandle(a(1)) && get(a(1),'Parent')~=0
    for ax = a(:)'
       fig = get(ax,'Parent');
-      set(0,'CurrentFigure',fig);
-      set(fig,'CurrentAxes',ax);
    end
 elseif evalin('caller','exist(''fh'',''var'')')
    fh = evalin('caller','fh');
-   if ~exist('f','var') || isempty(f);   f = numel(fh.f);   end
-
-   set(fh.f(f),'CurrentAxes',fh.a(f).h(a));
+   if isempty(varargin) || isempty(varargin{1}) || ~isnumeric(varargin{1})
+   	  f = numel(fh.f);
+   else f = varargin{1}; vstart = 2;
+   end
+   fig = fh.f(f);
+   ax  = fh.a(f).h(a);
+else
+	fig = gcf;
+	ax  = gca;
 end
+
+set(0,'CurrentFigure',fig);
+set(fig,'CurrentAxes',ax);
 hold on;
 
+for v = vstart:2:numel(varargin)
+	ax.(varargin{v}) = varargin{v+1};
+end
--- other/azbox/matchy.m
+++ other/azbox/matchy.m
@@ -1,46 +1,63 @@
-function matchy(ax,type)
+function matchy(varargin)
 % matchy.m: Force y-axis limits to match for a series of axes passed as
 % inputs
 % 
 % Ex.
 % matchy
 % matchy(ax)
-% matchy([], 'XLim')
-% matchy(ax, 'XLim')
-% matchy(ax,{'XLim','YLim'})
+% matchy('XLim')
+% matchy('x')
+% matchy('x','y')
+% matchy({'x','y'})
+% matchy(ax,{'x','y'})
 % matchy(ax,{'XLim','YLim','ZLim'})
+% matchy('c') % matches color axes
 % 
 % 2013-04-16 AZ Reverse-engineered to MC's code
 % 2014-01-13 AZ Added type input to choose which axes to match
 %               Now can pass figure handle to match all axes in figure
 % 2014-07-16 AZ Allows multiple types
+% 2020-05-28 AZ Made input handling way more flexible/intuitive
+%               Added support for color axis matching
 
-if ~exist('ax','var') || isempty(ax)
-   ax = gcf;
+%% Parse inputs
+type = {};
+for v = 1:nargin
+	if ishandle(varargin{v}), ax = varargin{v};
+	else
+		if ~iscell(varargin{v}), varargin{v} = {varargin{v}}; end
+		for i = 1:numel(varargin{v})
+			switch( upper(varargin{v}{i}) )
+				case {'X','Y','Z','C'}, type = [type [varargin{v}{i} 'Lim']];
+				otherwise 					type = [type  varargin{v}{i}       ];
+			end
+		end
+	end
 end
+
+%% Set defaults
+if isempty(type), type = {'XLim','YLim'}; end
+if ~exist('ax','var') || isempty(ax), ax = gcf; end
 
 if strcmpi(get(ax,'type'),'figure')
    ax = get(ax,'Children');
    ax = ax(strcmpi(get(ax,'Type'),'axes')); % choose only axes-type childrens
 end
 
-if ~exist('type','var') || isempty(type)
-   cellfun(@(x) matchy(ax,x),{'XLim','YLim'})
-   return
-elseif iscell(type)
-   cellfun(@(x) matchy(ax,x),type)
-   return
-end
 
+%% Initialize
 na = numel(ax);
-
 yy = zeros(na,2);
-for a = 1:na
-   yy(a,:) = get(ax(a),type);
-end
 
-newy = [min(yy(:,1)) max(yy(:,2))];
+%% Main loop
+for i = 1:numel(type)
+	for a = 1:na
+	   yy(a,:) = get(ax(a),type{i});
+	end
 
-for a = 1:na
-   set(ax(a),type,newy);
+	newy = [min(yy(:,1)) max(yy(:,2))];
+
+	for a = 1:na
+	   set(ax(a),type{i},newy);
+	end
 end
