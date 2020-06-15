413c9040bd11542ce75d641cadfe80a56539d186
Andrew Zaharia <az2522@columbia.edu>
2020-06-15 16:30:03 -0400

    simulate_stats: now saves all groundtruth and hyps Hyperspheres. cut extraneous sigtest and estimates. modified scenario 3 to be compatible with tests for scenarios 4 and 5--scenario generation is identical for these

--- demo/simulate_stats.m
+++ demo/simulate_stats.m
@@ -5,11 +5,24 @@ function [sigtest,estimates,hyps,groundtruth] = simulate_stats(nsamps)
 if ~exist('nsamps','var') || isempty(nsamps), nsamps = 100; end
 
 %% TODO: ALL TESTS WITH DIFFERENT ESTIMATORS?
-nboots = 100;
+nboots = 100;   % # of bootstraps during statistical testing
 %% Testing as a function of d, n, n-ratio, and r-ratio:
 ns = 2.^( 6:8); % # samples to test
 ds = 2.^( 1:5); % # dimensions
 rs = 2.^(-1:3); % # radii
+
+r  = 2;
+
+sigfield = {'sig',     'sigdiff'};
+measures = {'overlap', 'distance', 'radius difference', ...
+            'overlap difference', 'distance difference'};
+mnames   = {'overlap', 'distances2CrossValidated', 'radii', ...
+            'overlap', 'distances2CrossValidated'};
+
+nn = numel(ns);
+nd = numel(ds);
+nm = numel(measures);
+
 s  = 3;         % scenario chosen from the following:
 % Different scenarios needing testing:
 % (1) 2 balls with 0 overlap.
@@ -17,35 +30,22 @@ s  = 3;         % scenario chosen from the following:
 % (3) 2 balls with same (non-zero) radius.
 % (4) 3 balls, 2 with same (non-zero) overlaps
 % (5) 3 balls, 2 with same (non-zero) distances
-
-r  = 2; 
-
-sigfield = {'sig',     'sigdiff'};
-measures = {'overlap', 'distance', 'radius difference', ...
-            'overlap difference', 'distance difference'};
-mnames   = {'overlap', 'distances2CrossValidated', 'radii', ...
-            'overlap', 'distances2CrossValidated'};
-simfile  = sprintf('statsim_%u%s.mat',s,measures{s}(1:2));
-
-nn = numel(ns);
-nd = numel(ds);
-nm = numel(measures);
-
 % Can combine scenarios:
 % (a) is (1) and (2) in 3 balls. Balls 1 and 2 have 0 distance, ball 3 placed
 %    to have 0 overlap with ball 1.
 % ... though this gets annoying if playing with r- and n-ratios.
 
 % Initialize data for saving (could probably change from cell to tensor)
-nc2   	 = 1+2*double(s>2);
-sigtest   = repmat({NaN(nn,nsamps,nc2)},[nm nd]);
+nc2     = 1+2*double(s>2);
+sigtest = repmat({NaN(nn,nsamps)},[nm nd]);
+simfile = sprintf('statsim_%u%s.mat',s,measures{s}(1:2));
 
 if exist(simfile,'file'), load(simfile); fprintf('Loaded simulations.\n'); end
 
 DISPLAYED = false;
 for d = 1:nd
 	% Generate points for the different scenarios
-	groundtruth = generateScenario(s,ds(d),rs(r));
+	groundtruth{s,d,r} = generateScenario(s,ds(d),rs(r));
 
    for n = 1:nn
    	if any(isnan(vectify(sigtest{s,d}(n,:,:))))
@@ -54,30 +54,31 @@ for d = 1:nd
    			DISPLAYED = true;
    		end
 		   % Simulate points
-	      [points,groundtruth] = groundtruth.sample([ns(n) ns(n)*ones(1,1+double(s>2))],nsamps);
-	      hyps(d,n,:) = Hypersphere('estimate',points,groundtruth.categories,'independent');%.meanAndMerge(true);
+	      [points,groundtruth{s,d,r}] = groundtruth{s,d,r}.sample([ns(n) ns(n)*ones(1,1+double(s>2))],nsamps);
+	      hyps{s}(d,n,:) = Hypersphere('estimate',points,groundtruth{s,d,r}.categories,'independent');%.meanAndMerge(true);
 
 	      % Assess significance on samples
-	      tmp = NaN(size(sigtest{s,d},[2 3]));
+	      tmp = NaN(nsamps,nc2);
 	      parfor b = 1:nsamps
-	         tmp(b,:) = SetOfHyps(hyps(d,n,b)).significance(...
+	         tmp(b,:) = SetOfHyps(hyps{s}(d,n,b)).significance(...
 	                            points(:,:,b),nboots).(sigfield{1+double(s>2)}).(mnames{s}(1:2));
 	      end
-		   sigtest{s,d}(n,:,:) = tmp;
+	      sigtest{s,d}(n,:) = tmp(:,1+2*double(s==3)); % pick 3rd comparison only when s==3
 	      stationarycounter([d n],[nd nn])
 	      % save in-progress fits
-	      save(simfile,'hyps','sigtest')
+	      save(simfile,'hyps','sigtest','groundtruth')
 	   end
    end
 end
 
 %% Extract data: collect estimates (e.g., overlaps, distances)
-estimates = repmat({NaN(nn,nsamps,nc2)},[nm nd]);
+estimates = repmat({NaN(nn,nsamps)},[nm nd]);
 for d = 1:nd
 	for n = 1:nn
       switch s
-      	case {1,2};   estimates{s,d}(n,:) =              hyps(d,n,:).(mnames{s});
-      	case {3,4,5}; estimates{s,d}(n,:) = diff(vertcat(hyps(d,n,:).(mnames{s})),[],2);
+      	case {1,2}; estimates{s,d}(n,:) =                     hyps{s}(d,n,:).(mnames{s});
+      	case 3;     estimates{s,d}(n,:) = diff(indexm(vertcat(hyps{s}(d,n,:).(mnames{s})),[],2:3),[],2);
+   		case {4,5}; estimates{s,d}(n,:) = diff(indexm(vertcat(hyps{3}(d,n,:).(mnames{s})),[],1:2),[],2);
    	end
    end
 end
@@ -85,14 +86,17 @@ end
 %% Plot analyses
 nShown = [1 nn  1 nn];
 dShown = [1  1 nd nd];
+rShown = 2;
 
 fh = newfigure([3 3],measures{s});
 ax = fh.a.h([4 5 7 8]);
 
 for i = 1:3
-   sampsz = 2.^[i+4 i+4];
-   groundtruth.plotSamples(sampsz,fh.a.h(i));
-   title(sprintf('Samples: n_1= %u, n_2= %u',sampsz))
+   sampsz = (2^(i+4))*ones(1,2+double(s>2));
+   generateScenario(s,2,rs(rShown)).plotSamples(sampsz,fh.a.h(i));
+   if s<3, title(sprintf('Samples: n_1= %u, n_2= %u',         sampsz))
+   else    title(sprintf('Samples: n_1= %u, n_2= %u, n_3= %u',sampsz))
+   end
 end
 
 %% TODO: different colors for different conditions
@@ -140,19 +144,20 @@ function hyp = generateScenario(s,d,r)
 %    s: (scalar) the scenario number to choose
 %    d: (scalar) the dimensionality of the hyperspheres
 %    r: (scalar) the radius of one hypersphere (the other(s) is/are 1)
-
    switch s
       case 1 % OVERLAP. Null distribution: 0 overlap.
 			hyp = Hypersphere([zeros(1,d);r+1 zeros(1,d-1)],[r 1]);
       case 2 % INTER-CENTER DISTANCE. Null distribution: 0 distance.
-      	hyp = Hypersphere( zeros(2,d),                  [r 1]);
-   	case 3 % RADIUS DIFFERENCE. Null distribution: radii are the same. (Need 3+ balls.)
-   		hyp = Hypersphere([zeros(1,d);r+1 zeros(1,d-1);ones(1,d)],[r 1 1]);
+      	hyp = Hypersphere( zeros(2,d),[r 1]);
+   	case {3,4,5} % (Need 3+ balls.)
+   		% RADIUS DIFFERENCE. Null distribution: radii are the same.
+   		% OVERLAP DIFFERENCE. Null distribution: overlaps are the same.
+   		% INTER-CENTER DISTANCE DIFFERENCE. Null distribution: distances are the same.
+   		hyp = Hypersphere([zeros(1,d);r+0.75 zeros(1,d-1);0 r+0.75 zeros(1,d-2)],[r 1 1]);
+   		% hyp = Hypersphere([zeros(1,d);r+1 zeros(1,d-1);ones(1,d)],[r 1 1]);
    	% case 4 % OVERLAP DIFFERENCE. Null distribution: overlaps are the same. (Need 3+ balls.)
-   	% 	hyp = Hypersphere([zeros(1,d);r+1 zeros(1,d-1);ones(1,d)],[r 1 1]);
-   	% case 5 % INTER-CENTER DISTANCE DIFFERENCE. Null distribution: distances are the same.
-   	% 	    %    (Need 3+ balls.)
-   	% 	hyp = Hypersphere([zeros(1,d);r+1 zeros(1,d-1);ones(1,d)],[r 1 1]);
+   		% hyp = Hypersphere([zeros(1,d);3*r/2 zeros(1,d-1);0 3*r/2 zeros(1,d-2)],[r 1 1]);
    end
+end
 
 end
