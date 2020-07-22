afddd174412abaef360dcbdaec05a16fa8932d55
Andrew Zaharia <az2522@columbia.edu>
2020-07-22 18:58:07 -0400

    cvindex: changed random permutation index part of data structure to cell. can construct with 2 new options: (1) bootstrappedIndices (passed as 1st argument): enables you to generate cvindex objects that incorporate bootstraps (useful for keep bootstrapped data from duplicating the same point across folds) and (2) uniquedata: assumes you're using the cvindex object to index unique(data(boostrapIndices)). useful to keep memory usage down and only duplicate points in memory during cvindex.crossvalidate evaluation

--- tools/cvindex.m
+++ tools/cvindex.m
@@ -2,22 +2,51 @@ classdef cvindex
    properties
       nCV     % number of folds
       n       % number of samples
-      rp      % matrix of random permutation of indices organized by fold
+      rp      % cell of random permutation of indices organized by fold
+      uniquedata = false; % whether indexing is applied to unique data or not
    end
    methods
-      function obj = cvindex(n,nCV)     % Constructor
+      function obj = cvindex(n,nCV,uniquedata) % Constructor
+      % Note: can pass bootstrappedIndices in first argument instead of n
+         if exist('uniquedata','var') && ~isempty(uniquedata)
+            obj.uniquedata = uniquedata;
+         end
+
+         if numel(n) > 1 % bootstrappedIndices passed as first input
+            bootstrappedIndices = n(~~n); % strip zeros
+            hh = hist(bootstrappedIndices,single(min(bootstrappedIndices):max(bootstrappedIndices)));
+            if obj.uniquedata
+               density = hh(~~hh);
+               % New n
+               n = numel(density); %same as sum(~~unique(bootstrappedIndices));
+            else
+               density = hh;
+            end
+         end
+
+         % Construct random permutations (rp) of indices and separate into nCV folds
+         rp = reshape_sloppy(randperm(n),nCV);
+         % Strip NaN's and convert to cell
+         rp = cellfun(@(x) x(~isnan(x)), num2cell(rp,1),'UniformOutput',false);
+
+         % Density-based expansion of indices (re-applying bootstrap)
+         if exist('density','var')
+            rp = cellfun(@(r) cell2mat_concat(...
+                 arrayfun(@(x) x*ones(density(x),1),r,'UniformOutput',false)...
+                                                 ),rp,'UniformOutput',false);
+         end
+
+         % Populate object
          obj.nCV = nCV;
          obj.n   = n;
-         obj.rp  = reshape_sloppy(randperm(n),nCV);
+         obj.rp  = rp;
       end
       function ix = train(obj,i)        % Output training set for i'th fold
          a = 1:obj.nCV;
-         ix = vectify(obj.rp(:,a(~ismembc(a,i)))); % faster than setdiff(a,i))
-         ix = ix(~isnan(ix));
+         ix = vectify(obj.rp{a(~ismembc(a,i))}); % faster than rp{setdiff(a,i)}
       end
       function ix = test(obj,i)         % Output test set for i'th fold
-         ix = obj.rp(:,i);
-         ix = ix(~isnan(ix));
+         ix = obj.rp{i};
       end
       function out = crossvalidate(obj,fcn,varargin)
       % Calculate fcn on training data in all folds
