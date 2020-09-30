3e13c4d3cb82db8fb94a2445474b89d787474de8
Andrew Zaharia <az2522@columbia.edu>
2020-09-30 17:53:30 -0400

    estimateHypersphere: parallelize independent frame estimates

--- niko/estimateHypersphere.m
+++ niko/estimateHypersphere.m
@@ -167,10 +167,15 @@ if exist('categories','var') && numel(categories.labels)>1
    %% EVALUATE ALL FRAMES IN SAME SPACE
    if f > 1
       if INDEPENDENT % normalize must be false (think about this some more)
-         for i = 1:f
-            [hyp(i),loc_cv{i}] = estimateHypersphere(points(:,:,i),categories,'raw',varargin{:});
-         end
-         if CVDISTS, loc_cv = reshape(cat(1,loc_cv{:}),[nCats f])'; end
+         if CVDISTS
+            parfor i = 1:f
+               [hyp(i),loc_cv{i}] = estimateHypersphere(points(:,:,i),categories,'raw',varargin{:});
+            end
+            loc_cv = reshape(cat(1,loc_cv{:}),[nCats f])'; end
+         else
+            parfor i = 1:f
+               hyp(i) = estimateHypersphere(points(:,:,i),categories,'raw',varargin{:});
+            end
          return
       end
       points  = reshape(permute(points,[1 3 2]),n*f,d);
