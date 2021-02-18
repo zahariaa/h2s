cf1216c6be10b6bf59bce6ce053952b455fb6c0b
Andrew Zaharia <az2522@columbia.edu>
2021-02-18 14:59:45 -0500

    testing radius difference permutation test

--- demo/simulate_stats.m
+++ demo/simulate_stats.m
@@ -71,7 +71,7 @@ nm = numel(measures);
 % keyboard
 
 basedir = '';%'/moto/nklab/users/az2522/';
-simfolder = '20210210/';%'20210203/';%'20210126/';%'20200929/';%'20201215/';%'20200929/';
+simfolder = '20210217/';%'20210210/';%'20210203/';%'20210126/';%'20200929/';%'20201215/';%'20200929/';
 simfolder = [basedir 'data/statsim/' simfolder];
 if ~exist(simfolder,'dir'), mkdir(simfolder); end
 simfile = @(s,d,r,n) sprintf('%s%ss%g_d%g_r%g_n%g.mat',simfolder,estimator,s,d,r,n);
